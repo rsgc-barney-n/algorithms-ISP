bb099c75b7cb0139b55e7101095210af0b162950
Noah Barney <student@MacBook-Pro-0670-NBarney.local>
2016-01-14 13:20:43 -0500

    Lunch time help with MR. Gordon
    implemented the quadratic equation into the program

--- command-line-example/main.swift
+++ command-line-example/main.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Cocoa
 
 // Print welcome message on it's own line
 print("Welcome to the quadratic equation solver!")
@@ -33,65 +34,27 @@ print("the actual response is: \(actualResponse)")
 print("What is your A value?")
 // Read A value
 
-var a  = Int(readLine(stripNewline: true)!)!
+var a  = Float(readLine(stripNewline: true)!)!
 
 
 print("a is \(a)")
 //Ask B value
 print("What is your B value?")
 //Read B value
-var b = Int (readLine(stripNewline: true)!)!
+var b = Float(readLine(stripNewline: true)!)!
 print("b is \(b)")
 //ask C value
 print("What is your C value?")
 // Read C value
-var c = Int (readLine(stripNewline: true)!)!
+var c = Float(readLine(stripNewline: true)!)!
 print("c is \(c)")
-var validInput : Bool          // Status of input
-repeat {
-    
-    // Show prompts
-    
-    var A = readLine(stripNewline: true)
-    var B = readLine(stripNewline: true)
-    var C = readLine(stripNewline: true)
-    //print("Select your favourite (A/B/C/Quit): ", terminator: "")
-    
-    // Get the user's input
-    var givenFoodPreference = readLine(stripNewline: true)
-    
-    // Assume valid input
-    validInput = true
-    
-    // Interpret the result, if one was given
-    if let foodPreference = givenFoodPreference {
-        
-        // Add a blank line to separate response from input block
-        print("")
-        
-        switch foodPreference {
-            
-        case "A", "a":
-            print("Awesome, I love Cheerios!")
-            
-        case "B", "b":
-            print("My favourite is mango.")
-            
-        case "C", "c":
-            print("Have you tried baked eggplant? Delectable!")
-            
-        case "Quit", "QUIT", "quit":
-            print("Bye.")
-            
-        default:
-            print("Sorry, I didn't get that.")
-            validInput = false
-        }
-        
-    }
-    
-    
-} while validInput == false
 
-// Print one additional blank line to separate from exit code message that is automatically displayed
-print("")
+//Implement quadrtic formula
+var x1 = -1*b + sqrt((b*b) - (4*(a)*(c)))
+var x2 = -1*b - sqrt((b*b) - (4*(a)*(c)))
+print("The Roots are \(x1)")
+print("and \(x2)")
+
+
+
+
