639f564c000d07f59e876571eb84136b2a5deced
Noah Barney <student@MacBook-Pro-0670-NBarney.local>
2016-01-15 15:32:34 -0500

    Made IF statement so if there is no roots it prints "no roots" also I figured out a way to implement vertex form into code

--- command-line-example/main.swift
+++ command-line-example/main.swift
@@ -1,16 +1,11 @@
 // Noah Barney
-//  main.swift
-//  basic-command-line
-//
-//  Created by Russell Gordon on 2015-12-16.
-//  Copyright © 2015 Royal St. George's College. All rights reserved.
-//
+// Quadratic Formula Solver
 
 import Foundation
 import Cocoa
 
 // Print welcome message on it's own line
-print("Welcome to the quadratic equation solver!")
+print("Welcome to the Quadratic Equation Solver")
 
 
 // Print prompt (but don't go to new line)
@@ -18,43 +13,64 @@ print("Are you giving the equation in vertex or standard form? ")
 
 // Get the response
 var input = readLine(stripNewline: true)
-
-// Say hello, so long as some input was provided
-// Add an extra newline at the start     \n
-var actualResponse = ""
-if let response = input {
-    print("Hey, you said \(response)")
-    actualResponse = response
+// create if statement to separate "vertex" form from the rest of the program
+if input == "vertex" { print("what is the equation")
+    print("for squared terms you must write it like x*x or(x)*(x)")
+    //y= a(x-h)^2+k
+    var vertex = Float(readLine(stripNewline: true)!)!
+    
+    
+    
+    
+    
+    
+    
 }
-print("the actual response is: \(actualResponse)")
-
-
-// ask A B and C values
-//Ask A value
-print("What is your A value?")
-// Read A value
-
-var a  = Float(readLine(stripNewline: true)!)!
-
-
-print("a is \(a)")
-//Ask B value
-print("What is your B value?")
-//Read B value
-var b = Float(readLine(stripNewline: true)!)!
-print("b is \(b)")
-//ask C value
-print("What is your C value?")
-// Read C value
-var c = Float(readLine(stripNewline: true)!)!
-print("c is \(c)")
-
-//Implement quadrtic formula
-var x1 = -1*b + sqrt((b*b) - (4*(a)*(c)))
-var x2 = -1*b - sqrt((b*b) - (4*(a)*(c)))
-print("The Roots are \(x1)")
-print("and \(x2)")
-
+    // use else if to allow the rest of the program to run if the input is "standard"
+else if input == "standard" {
+    // Say hello, so long as some input was provided
+    // Add an extra newline at the start \n
+    var actualResponse = ""
+    if let response = input{
+        print("Hey, you said \(response)")
+        actualResponse = response
+    }
+    print("the actual response is: \(actualResponse)")
+    
+    
+    // ask A B and C values
+    //Ask A value
+    print("What is your A value?")
+    // Read A value
+    
+    var a  = Float(readLine(stripNewline: true)!)!
+    
+    
+    print("a is \(a)")
+    //Ask B value
+    print("What is your B value?")
+    //Read B value
+    var b = Float(readLine(stripNewline: true)!)!
+    print("b is \(b)")
+    //ask C value
+    print("What is your C value?")
+    // Read C value
+    var c = Float(readLine(stripNewline: true)!)!
+    print("c is \(c)")
+    
+    
+    //Implement quadrtic formula
+    var x1 = -1*b + sqrt((b*b) - (4*(a)*(c)))
+    var x2 = -1*b - sqrt((b*b) - (4*(a)*(c)))
+    
+    if x1 == Float.NaN {
+        print( " There are No Roots")
+    }
+    print("The Roots are \(x1)")
+    print("and \(x2)")
+    
+}
+print("Hope the Quadratic Equation Solver Helped!")
 
 
 
